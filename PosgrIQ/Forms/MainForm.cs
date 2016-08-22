37d4b77b8f5b5f05cf63673c558344e0745ab5e5
crisguycabs <crisguycabs@gmail.com>
2016-08-22 09:22:24 -0500

    se elimino la libreria pdfsharp

--- PosgrIQ/Forms/MainForm.cs
+++ PosgrIQ/Forms/MainForm.cs
@@ -8,10 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.OleDb;
-using PdfSharp;
-using PdfSharp.Drawing;
-using PdfSharp.Pdf;
-using PdfSharp.Pdf.IO;
 using GemBox.Spreadsheet;
 
 namespace PosgrIQ
@@ -126,34 +122,6 @@ namespace PosgrIQ
                 // Print the CategoryID as a subscript, then the CategoryName:
                 Console.WriteLine("CategoryName[{0}] is {1}", dr[0], dr[1]);
             }
-        }
-
-        public void Test1()
-        {
-            // Create a new PDF document
-
-            PdfDocument document = new PdfDocument();
-
-            document.Info.Title = "Created with PDFsharp";
-
-            // Create an empty page
-
-            PdfPage page = document.AddPage();
-
-            // Get an XGraphics object for drawing
-            XGraphics gfx = XGraphics.FromPdfPage(page);
-
-            // Create a font
-            XFont font = new XFont("Verdana", 20, XFontStyle.BoldItalic);
-
-            // Draw the text
-            gfx.DrawString("Hello, World!", font, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.TopCenter);
-
-            // Save the document...
-            const string filename = "HelloWorld.pdf";
-            document.Save(filename);
-            // ...and start a viewer.
-            System.Diagnostics.Process.Start(filename);
         }
 
         public void TestGembox()
